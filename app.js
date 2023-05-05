6e7890f6d81370d4c32c20376c90b884df56dc1f
Barik <fikrialbariq01@mail.ugm.ac.id>
2023-05-06 03:06:02 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    
+  res.sendFile(__dirname + "/public/index.html")
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Server is up and listening at port: ${PORT}`)
+})
